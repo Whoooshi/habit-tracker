f3036111a8b58241399c8369b95957e724d3a7d8
Whoooshi <nmichal3@gmail.com>
2022-01-15 08:35:37 +0100

    main screen + new habit form

---
+++ app/script/app.js
@@ -0,0 +1,9 @@
+/* --------- */
+/* my Habits */
+/* --------- */
+let healthHabits = [];
+let relationsHabits = [];
+let workHabits = [];
+let financesHabits = [];
+let hobbyHabits = [];
+let proHabits = [];
---
+++ app/script/newHabit.js
@@ -0,0 +1,67 @@
+/* ---------------- */
+/* Adding new habit */
+/* ---------------- */
+let nextBtnsArr = [
+    document.getElementById('next-1'),
+    document.getElementById('next-2'),
+    document.getElementById('next-3')
+];
+
+let prevBtnsArr = [
+    document.getElementById('prev-2'),
+    document.getElementById('prev-3'),
+    document.getElementById('prev-4')
+];
+
+const screenOne = document.querySelector('.newHabitForm-categoryEvaluation');
+const screenTwo = document.querySelector('.newHabitForm-habitDefinitionYesNo');
+const screenThree = document.querySelector('.newHabitForm-habitDefinitionNumeric');
+const screenFour = document.querySelector('.newHabitForm-habitFrequency');
+
+const screens = [
+    document.getElementById("screen-1"),
+    document.getElementById("screen-2"),
+    document.getElementById("screen-3"),
+    document.getElementById("screen-4")
+];
+
+let screenId = 1;
+
+function dBlockAdd(screenId) {
+    screens[screenId-1].classList.add('d-block');
+}
+
+function dBlockRemove(screenId) {
+    screens[screenId-1].classList.remove('d-block');
+}
+
+function increaseId() {
+    return screenId++;
+}
+
+function decreaseId() {
+    return screenId--;
+}
+
+nextBtnsArr.forEach(function(item) {
+    item.addEventListener("click", function() {
+        increaseId();
+        dBlockAdd(screenId);
+        dBlockRemove(screenId-1);
+    });
+})
+
+prevBtnsArr.forEach(function(item) {
+    item.addEventListener("click", function() {
+        decreaseId();
+        dBlockRemove(screenId+1);
+        dBlockAdd(screenId);
+    });
+})
+
+function myFunction() {
+    let myValue = document.forms['newHabitForm']['habitCategory'];
+    if (myValue.value === 'work') {
+        console.log("robota");
+    }
+}
